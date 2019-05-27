3e59a116d7288117be3435929fb5bdb788bac498
Thom Lappas <tlappas@gmail.com>
2019-05-27 11:40:40 -0400

    Add functions to remap labels and then split into stratified train/test datasets.

--- src/pipeline/build_test_train.py
+++ src/pipeline/build_test_train.py
@@ -1,2 +1,8 @@
-def load_data(path):
-    pass
+from sklearn.model_selection import test_train_split
+
+#Assumes labels are yelp ratings [1,2,3,4,5]. Remap must be a list of length 5.
+def remap_labels(labels, new_labels):
+    return [new_labels[label-1] for label in labels]
+
+def split(all_data, labels):
+    return x_train, x_test, y_train, y_test = test_train_split(all_data, test_size=0.3, stratify=labels)
